9ff144022afb1b69ed449fb7b0df39161032da47
Nandyala Akshaya <akshayanandyala23@gmail.com>
2026-09-13 17:32:13 +0530

    Allow deployed frontend in CORS

--- netshield-backend/main.py
+++ netshield-backend/main.py
@@ -25,7 +25,9 @@ from risk_report import generate_risk_report
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000","http://127.0.0.1:3000"],
+    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000",
+    "https://network-anomaly-detection-threat-qhyz.onrender.com",
+],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
